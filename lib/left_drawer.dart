888e1b80bc2dce42573aa9e6624ca35ba3a4e0bb
caturwmn <caturwira41@gmail.com>
2023-12-04 13:22:39 +0700

    ugh

---
+++ lib/left_drawer.dart
@@ -0,0 +1,55 @@
+import 'package:ancestralreads/menu.dart';
+import 'package:flutter/material.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.indigo,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Ancestral Reads',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                // Text("TBA",
+                //   textAlign: TextAlign.center,
+                //   style: TextStyle(
+                //       fontSize: 15,
+                //       fontWeight: FontWeight.normal,
+                //       color: Colors.white
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Halaman Utama'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(),
+                  ));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
